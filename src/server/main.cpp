aad02617b80c6f2c2cd5ea293a2b0bf9c4f24164
Ivan Komissarov <ABBAPOH@gmail.com>
2015-04-14 19:23:29 +0300

    Add debug output

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -8,7 +8,7 @@ Server *_server = 0;
 
 static void sigIntSandler(int)
 {
-    std::cerr << "sigint" << std::endl;
+    std::cerr << "Stopping server" << std::endl;
     _server->stop();
 }
 
@@ -19,21 +19,27 @@ int main(int argc, char *argv[])
 
     Server server;
     _server = &server;
+    std::cout << "Initializing server" << std::endl;
     auto ok = server.init();
     if (!ok) {
         std::cerr << "Can't init server:" << ok.errorString() << std::endl;
         return 1;
     }
 
+    std::cout << "Starting server" << std::endl;
     ok = server.start();
     if (!ok) {
         std::cerr << "Can't init server:" << ok.errorString() << std::endl;
         return 1;
     }
 
+    std::cout << "Waiting for interrupt" << std::endl;
+
     signal(SIGINT, sigIntSandler);
 
     server.waitForFinished();
+
+    std::cout << "Server stopped" << std::endl;
 
     return 0;
 }
